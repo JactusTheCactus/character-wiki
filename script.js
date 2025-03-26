2ca17f0758116493da2da0f7b09099b575bd464b
JactusTheCactus <threateninglyamusing@gmail.com>
2025-03-26 15:13:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ document.addEventListener("DOMContentLoaded", async function () {
         const list = document.getElementById("character-list");
         characters.forEach((character, index) => {
             const fullName = [character.first_name, character.middle_name, character.last_name].filter(Boolean).join(' ');
-            console.log(`Index: ${index}; Name: ${fullName};`);
+            console.log();
+            console.log(fullName);
+            console.log(index);
             const li = document.createElement("li");
             li.className = "p-3 bg-gray-200 rounded hover:bg-gray-300 transition";
             li.innerHTML = `<a href="character.html?index=${index}" class="block text-lg text-gray-800">${fullName}</a>`;
